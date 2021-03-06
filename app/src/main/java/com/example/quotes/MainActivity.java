db8db58a7dbc98c5f23ffae2945da3aef08718a8
Vishal <vishaldel1234@gmail.com>
2021-03-07 01:10:25 +0530

    minor fixes

--- app/src/main/java/com/example/quotes/MainActivity.java
+++ app/src/main/java/com/example/quotes/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.quotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button shareBtn;
+    TextView quotes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        shareBtn=findViewById(R.id.btnShare);
+        quotes=findViewById(R.id.quoteText);
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text=quotes.getText().toString();
+                Intent intent=new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(intent.EXTRA_TEXT,text);
+                startActivity(intent);
+               
+            }
+        });
     }
 }
